19c6c55e03f5ef1418cea95b7d11fc58de763e82
HansRobrecht <79218348+HansRobrecht@users.noreply.github.com>
2021-05-07 16:08:28 +0200

    Setting up login

--- js/launcher.js
+++ js/launcher.js
@@ -6,23 +6,48 @@
     
         window.addEventListener('load', function() {
 
-            const lookupProfile = function(){
-                const email = document.getElementById('email').value;
-                fetch('http://localhost:3000/feedback')
+            const validateForm = function(){
+                return (document.getElementById('email').value !== '' && document.getElementById('password').value !== '');
+            };
+
+            const lookupProfile = function(specificButton){
+                if(validateForm()){
+                    const email = document.getElementById('email').value.toLowerCase();
+                    let record;
+                    console.log(email);
+                    fetch(`https://api.airtable.com/v0/appAMnFrTLj28QYno/Profiles?filterByFormula={Email}="${email}"`, {
+                        headers:{
+                            'Authorization' : 'Bearer keyj9zMtJACYaD2uv'
+                        }
+                    })
                     .then(response => response.json())
-                    .then(response => console.log(response))
-                    .then(data => rows = data)
-                    .then(console.log(rows))
+                    .then(json => specificButton === 'login' ? validatePassword(json) : createNewProfile(json))
+                }
+                else{
+                    console.log('please fill all inputfields in');
+                }
             }
 
+            const createNewProfile = function(record){
+                if(record.records.length === 0){
+                    console.log('creating profile')
+                }
+            };
+
+            const validatePassword = function(record){
+                if(record.records.length !== 0){
+                    console.log('checking profile');
+                }
+            };
+        
             document.getElementById('login').addEventListener('click', (e) => {
                 e.preventDefault();
-                window.test();
+                lookupProfile('login');
             })
 
             document.getElementById('sign up').addEventListener('click', (e) => {
                 e.preventDefault();
-                lookupProfile()
+                lookupProfile('signup');
             })
     
         });
