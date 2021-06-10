2bc7cc211b7e283d3396fc00879c90a17496fff3
HansRobrecht <79218348+HansRobrecht@users.noreply.github.com>
2021-06-11 01:51:22 +0200

    Basic CSS Main Menu

--- js/mainMenu.js
+++ js/mainMenu.js
@@ -9,6 +9,7 @@
         let profile = JSON.parse(window.sessionStorage.getItem('loggedUser'));
 
         const setupPlayerStatistics = function(){
+            document.getElementById('username').innerText = profile.username;
             document.getElementById('gameCount').innerText = profile.totalGames;
             document.getElementById('avgScore').innerText = Math.round(profile.avgScore * 100) / 100;
             document.getElementById('maxScore').innerText = profile.maxScore;
