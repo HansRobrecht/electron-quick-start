405450089af80a31714190b57c686b6ce637d503
HansRobrecht <79218348+HansRobrecht@users.noreply.github.com>
2021-06-11 00:38:31 +0200

    Added Roboto, split js and changed banner backgroundimage

--- website/js/common.js
+++ website/js/common.js
@@ -1,42 +1,12 @@
 
 
 ;(function() {
-
-    // HET IS MOGELIJK OM VIA RECHTS EN ONDER BUITEN SCHERM TE LOPEN
     
         'use strict';
     
         window.addEventListener('load', function() {
 
             let toggleNavigation = false;
-            
-            const retrieveMaxRecords = function(){
-                const queryString = '?fields%5B%5D=MaxScore&fields%5B%5D=Username&maxRecords=5&sort%5B0%5D%5Bfield%5D=MaxScore&sort%5B0%5D%5Bdirection%5D=desc';
-                fetch('https://api.airtable.com/v0/appAMnFrTLj28QYno/Profiles' + queryString, {
-                            headers:{
-                                'Authorization' : 'Bearer keyj9zMtJACYaD2uv'
-                            }
-                        })
-                        .then(response => response.json())
-                        .then(json => setupMaxScores(json))
-            };
-
-            const setupMaxScores = function(records){
-                const scoreBoard = document.querySelector('main .sideContent');
-                for(let record of records.records){
-                    let topScorer = document.createElement('div');
-                    topScorer.classList.add('top5');
-
-                    let username = document.createElement('h3');
-                    username.innerText = record.fields.Username;
-
-                    let score = document.createElement('p');
-                    score.innerText = 'Score : ' +  record.fields.MaxScore;
-                    topScorer.appendChild(username);
-                    topScorer.appendChild(score);
-                    scoreBoard.appendChild(topScorer);
-                }
-            }
 
             function resizeEvent() {
                 if (window.innerWidth >= 769) {
@@ -45,6 +15,7 @@
                 }
                 else{
                     document.querySelector('header nav').style.display = 'none';
+                    document.getElementById('hamburgerButton').style.backgroundColor = '#f2dcb3';
                 }
             }
             
@@ -55,15 +26,15 @@
                 if(!toggleNavigation){
                     navigation.style.display = 'flex';
                     toggleNavigation = true;
+                    document.getElementById('hamburgerButton').style.backgroundColor = '#f2b33d';
                 }  
                 else{
                     navigation.style.display = 'none';
                     toggleNavigation = false;
+                    document.getElementById('hamburgerButton').style.backgroundColor = '#f2dcb3';
                 }
 
             });
-
-            retrieveMaxRecords();
 
         });
 
---
+++ website/js/index.js
@@ -0,0 +1,43 @@
+
+
+;(function() {
+    
+        'use strict';
+    
+        window.addEventListener('load', function() {
+            
+            const retrieveMaxRecords = function(){
+                const queryString = '?fields%5B%5D=MaxScore&fields%5B%5D=Username&maxRecords=5&sort%5B0%5D%5Bfield%5D=MaxScore&sort%5B0%5D%5Bdirection%5D=desc';
+                fetch('https://api.airtable.com/v0/appAMnFrTLj28QYno/Profiles' + queryString, {
+                            headers:{
+                                'Authorization' : 'Bearer keyj9zMtJACYaD2uv'
+                            }
+                        })
+                        .then(response => response.json())
+                        .then(json => setupMaxScores(json))
+            };
+
+            const setupMaxScores = function(records){
+                const scoreBoard = document.querySelector('main .sideContent .scoreboard');
+                let counter = 1;
+                for(let record of records.records){
+                    let topScorer = document.createElement('div');
+                    topScorer.classList.add('top5');
+
+                    let username = document.createElement('h3');
+                    username.innerText = `${counter} - ` + record.fields.Username;
+
+                    let score = document.createElement('p');
+                    score.innerText = 'Score : ' +  record.fields.MaxScore;
+                    topScorer.appendChild(username);
+                    topScorer.appendChild(score);
+                    scoreBoard.appendChild(topScorer);
+                    counter++;
+                }
+            }
+
+            retrieveMaxRecords();
+
+        });
+
+    })();
