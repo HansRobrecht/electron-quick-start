f2abbe64312279c36cbeec27425e8f8ddde9c466
HansRobrecht <79218348+HansRobrecht@users.noreply.github.com>
2021-06-11 01:37:53 +0200

    Added personal statistics feature on homepage website

--- website/js/index.js
+++ website/js/index.js
@@ -17,6 +17,42 @@
                         .then(json => setupMaxScores(json))
             };
 
+            const retrieveStatistics = function(){
+                const inputEmail = document.getElementById('emailInput').value;
+                console.log(inputEmail);
+                const queryString = `?filterByFormula={Email}="${inputEmail}"`;
+                fetch('https://api.airtable.com/v0/appAMnFrTLj28QYno/Profiles' + queryString, {
+                            headers:{
+                                'Authorization' : 'Bearer keyj9zMtJACYaD2uv'
+                            }
+                        })
+                        .then(response => response.json())
+                        .then(json => validateResults(json.records))
+            };
+
+            const validateResults = function(records){
+                if(records.length !== 0){
+                    const data = records[0].fields;
+                    if(data.Password === document.getElementById('passwordInput').value){
+                        processStatistics(data);
+                    }
+                    else{
+                        document.getElementById('invalidPassword').style.display = 'inline-block';
+                    }
+                }
+                else{
+                    document.getElementById('invalidEmail').style.display = 'inline-block';
+                }
+            };
+
+            const processStatistics = function(statistics){
+                document.querySelector('.stats').style.display = 'block';
+                document.getElementById('usernamePlayer').innerText = statistics.Username;
+                document.getElementById('totalGamesPlayer').innerText = statistics.TotalGames;
+                document.getElementById('avgScorePlayer').innerText = statistics.MaxScore;
+                document.getElementById('maxScorePlayer').innerText = Math.round(statistics.AverageScore * 100) / 100;
+            };
+
             const setupMaxScores = function(records){
                 const scoreBoard = document.querySelector('main .sideContent .scoreboard');
                 let counter = 1;
@@ -37,6 +73,13 @@
             }
 
             retrieveMaxRecords();
+
+            document.getElementById('searchStats').addEventListener('click', (e) =>{
+                e.preventDefault();
+                document.getElementById('invalidEmail').style.display = 'none';
+                document.getElementById('invalidPassword').style.display = 'none';
+                retrieveStatistics();
+            });
 
         });
 
